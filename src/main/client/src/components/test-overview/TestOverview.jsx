a44ede0ef5c34b3ec0fc425201ca3d592f2fc163
Austin <austinobejo@gmail.com>
2020-02-17 17:48:39 -0500

    Added basic layout for test overview and test results details

---
+++ src/main/client/src/components/test-overview/TestOverview.jsx
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import NavigationBar from "./../navbar/NavigationBar";
+import { Table } from "react-bootstrap";
+import './TestOverview.scss';
+
+export default class TestOverview extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedTest: null
+		}
+		this.testData = {
+			tests: [
+				{
+					name: "Test 1",
+					desc: "Test 1 Desc",
+					dateCreated: "2020-02-13",
+					dateLastTaken: "2020-02-14",
+					editable: false,
+					accessHavers: [
+						{
+							name: "John Doe",
+							email: "JohnDoe@a.com",
+							score: "7/10",
+							dateLastTaken: "2020-02-12",
+							numAttempts: "1"
+						},
+						{
+							name: "Dorithy Smildinger",
+							email: "JohnDoe@a.com",
+							score: "10/10",
+							dateLastTaken: "2020-02-12",
+							numAttempts: "1"
+						}
+					]
+				},
+				{
+					name: "Test 2",
+					desc: "Test 2 Desc",
+					dateCreated: "2020-02-05",
+					dateLastTaken: "2020-02-04",
+					editable: true,
+					accessHavers: [
+						{
+							name: "John Doe",
+							email: "JohnDoe@a.com",
+							score: "7/10",
+							dateLastTaken: "2020-02-12",
+							numAttempts: "1"
+						},
+						{
+							name: "Dorithy Smildinger",
+							email: "JohnDoe@a.com",
+							score: "10/10",
+							dateLastTaken: "2020-02-12",
+							numAttempts: "1"
+						}
+					]
+				}
+			]
+		}
+
+		this.selectTest = this.selectTest.bind(this);
+	}
+
+	selectTest(test) {
+		this.setState({ selectedTest: test })
+	}
+	render() {
+		return (
+			<React.Fragment>
+				<NavigationBar />
+				<div id={"parent"} className={"flex"}>
+					<div id={"left"}>
+						<div id={"title-left"}>
+							<div className={"title"}>Your Tests</div>
+							<button>New Test</button>
+						</div>
+						<Table striped hover>
+							<thead>
+								<tr>
+									<th>Test Name</th>
+									<th>Test Description</th>
+									<th>Date Published</th>
+									<th>Date Last Taken</th>
+									<th></th>
+									<th></th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.testData.tests.map((item) => {
+
+									return (<tr>
+										<td>{item.name}</td>
+										<td>{item.desc}</td>
+										<td>{item.dateCreated}</td>
+										<td>{item.dateLastTaken}</td>
+										<td>Delete</td>
+										<td>{item.editable ? "Edit" : "Cant Edit"}</td>
+										<td onClick={this.selectTest.bind(this, item)}>></td>
+									</tr>)
+								})}
+							</tbody>
+						</Table>
+						{/* <div>People Given Access</div>
+						<Table striped bordered hover>
+							<thead>
+								<tr>
+									<th>Test Name</th>
+									<th>Test Description</th>
+									<th>Date Published</th>
+									<th>Date Last Taken</th>
+									<th></th>
+									<th></th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.testData.tests.map((item) => {
+
+									return (<tr>
+										<td>{item.name}</td>
+										<td>{item.desc}</td>
+										<td>{item.dateCreated}</td>
+										<td>{item.dateLastTaken}</td>
+										<td>Delete</td>
+										<td>{item.editable ? "Edit" : "Cant Edit"}</td>
+										<td onClick={this.selectTest.bind(this, item)}>></td>
+									</tr>)
+								})}
+							</tbody>
+						</Table> */}
+					</div>
+
+					<div id={"right"}>
+						<div id={"title-right"}>
+							<div className={"title"}>People Given Access</div>
+						</div>
+						<Table striped bordered hover>
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Email</th>
+									<th>Total Score</th>
+									<th>Date Last Taken</th>
+									<th>Num Attempts</th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.selectedTest != null ? this.state.selectedTest.accessHavers.map((item) => {
+									return (<tr>
+										<td>{item.name}</td>
+										<td>{item.email}</td>
+										<td>{item.score}</td>
+										<td>{item.dateLastTaken}</td>
+										<td>{item.numAttempts}</td>
+										<td >Details</td>
+									</tr>)
+								}) : "asdf"}
+								{this.state.selectedTest != null ? <tr><td>Add new person</td><td>Email: </td></tr> : ""}
+							</tbody>
+						</Table>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
---
+++ src/main/client/src/components/test-results-detailed/TestResultsDetailed.jsx
@@ -0,0 +1,248 @@
+import React, { Component } from 'react';
+import NavigationBar from "./../navbar/NavigationBar";
+import { Button, Table } from "react-bootstrap";
+import './TestResultsDetailed.scss';
+
+export default class TestResultsDetailed extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedResults: null,
+			selectedDrinks: null,
+			data: {
+				realname: "John Doe",
+				testname: "Test 1",
+				attempts: [
+					{
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}, {
+						date: "2020-02-14",
+						totalScore: "1/10",
+						//array of each drink the person was supposed to make and their score for that part
+						scoreBreakdown: [
+							{
+								recipeName: "Whisky Sour",
+								originalRecipe: {},
+								submittedRecipe: {},
+								problems: [],
+								score: "1/10"
+							}
+						]
+					}
+				]
+			}
+		}
+		this.selectTest = this.selectTest.bind(this);
+	}
+
+	selectTest(test) {
+		this.setState({ selectedResults: test })
+	}
+	render() {
+		return (
+			<React.Fragment>
+				<NavigationBar />
+				<div id={"top"}>
+					<div className={"alightLeft"}>
+						<Button>Back</Button>
+					</div>
+					<div id={"title"}>
+						{this.state.data.realname}'s Results For {this.state.data.testname}
+					</div>
+				</div>
+				<div id={"parent"}>
+
+					<div id={"left-panel"}>
+						<Table striped hover>
+							<thead>
+								<tr>
+									<th>Date Taken</th>
+									<th>Best Score</th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.data.attempts.map((item) => {
+									return (<tr>
+										<td>{item.date}</td>
+										<td>{item.totalScore}</td>
+										<td><button onClick={this.selectTest.bind(this, item)}>Details</button></td>
+									</tr>)
+								})}
+							</tbody>
+						</Table>
+					</div>
+					<div id={"right-panel"}>
+						{this.state.selectedResults != null ? this.state.selectedResults.scoreBreakdown.map((item) => {
+							return (
+								<div>
+									{/* <div>Drink: {item.recipeName}</div>
+									<div>Score: {item.recipeName}</div>
+									<div>Problems: {item.problems}</div> */}
+									<thead>
+										<tr>
+											<td>Drink: {item.recipeName}</td>
+											<td>Score: {item.recipeName}</td>
+											<td>Problems: {item.problems}</td>
+										</tr>
+									</thead>
+									{/* <Table striped hover>
+										<thead>
+											<tr>
+												<th>Score</th>
+												<th>Problems</th>
+												<th>Expected Ingredients</th>
+												<th>Expected Ingredients</th>
+											</tr>
+										</thead>
+										<tbody>
+											{this.selectedResults != null ? this.testData.tests.map((item) => {
+
+												return (<tr>
+													<td>{item.name}</td>
+													<td>{item.desc}</td>
+													<td>{item.dateCreated}</td>
+													<td>{item.dateLastTaken}</td>
+													<td><button>Delete</button></td>
+													<td>{item.editable ? "Edit" : "Cant Edit"}</td>
+													<td onClick={this.selectTest.bind(this, item)}>></td>
+												</tr>)
+											}) : ""}
+										</tbody>
+									</Table> */}
+								</div>
+
+							)
+						}) : (
+								<Table striped hover>
+									<thead>
+										<tr>
+											<th>Score</th>
+											<th>Problems</th>
+											<th>Expected Ingredients</th>
+										</tr>
+									</thead>
+								</Table>
+							)}
+
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
