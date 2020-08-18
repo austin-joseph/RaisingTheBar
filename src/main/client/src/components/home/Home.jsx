ce0927315ec73411a03a8f620ca3e3928ed1288e
Austin <austinobejo@gmail.com>
2020-08-18 15:44:42 -0400

    Finished conversion

--- src/main/client/src/components/home/Home.jsx
+++ src/main/client/src/components/home/Home.jsx
@@ -19,14 +19,14 @@ export default class Home extends Component {
             <div className="title">Workshop</div>
             <div className="body">The workshop consists of all the practice and testing simulations that have been created by the user. When a simulation is selected, the user will be redirected to that simulation.
 Users are also allowed to search for a simulation by name and view shareable links for a specific simulation.</div>
-            <a className="btn btn-info" href="/workshop">Workshop</a>
+            <a className="btn btn-info" href="./workshop">Workshop</a>
           </div>
 
           <div className="center website_function">
             <div className="title">Creation Suite</div>
             <div className="body">The creation suite allows users to create a new simulation. To create a new simulation, users add the recipes they want the simulation to consist of
         from the recipe book. Users are also allowed to edit and view who has completed an existing simulation.</div>
-            <a className="btn btn-info" href="/creator">Creation Suite</a>
+            <a className="btn btn-info" href="./creator">Creation Suite</a>
           </div>
         </div>
 
